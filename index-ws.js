666a884f622cff5a7b8a0ee9a44fd239beda07a6
dan <daniel.couzens@epimorphics.com>
2023-10-27 07:22:28 +0000

    websocket

--- index-ws.js
+++ index-ws.js
@@ -11,4 +11,30 @@ server.on('request', app);
 
 server.listen(PORT, function () { console.log('Listening on ' + PORT); });
 
+/** Begin websocket */
 
+const WebSocketServer = require('ws').Server;
+
+const wss = new WebSocketServer({server: server});
+
+wss.on('connection', function connection(ws) {
+	const numClients = wss.clients.size;
+	console.log('Clients connected', numClients);
+
+	wss.broadcast(`Current visitors ${numbClients}`);
+
+	if (ws.readyState === ws.Open) {
+		ws.send('Welcome to my server');
+
+
+	}
+	ws.on('close', function close() {
+		console.log('A client has disconnected');
+	});
+});
+
+wss.broadcast = function broadcast(data) {
+	wss.clients.forEach(function each(client) {
+		client.send(data);
+	});
+}
